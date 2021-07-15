e8ab28a0567ea109cb9a5eb344de44fa27d73941
pmzulian <56852432+pmzulian@users.noreply.github.com>
2021-07-15 19:30:59 -0300

    Instalación mongoose y conexión a base de datos

--- src/db.js
+++ src/db.js
@@ -1,22 +1,21 @@
-const knex = require("knex")({
-  client: "mysql",
-  connection: {
-    host: "localhost",
-    user: "pmzulian",
-    password: "30212506",
-    database: "coderhouse",
-  },
-  pool: { min: 0, max: 8 },
-});
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://localhost:27017/ecommerce";
+
+async function CRUD() {
+    try {
+        
+        /* Conexión hacia la base de datos */
+        await mongoose.connect(MONGO_URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        console.log("Conexión con Mongoose exitosa")
+
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
-const sqlite = require("knex")({
-  client: "sqlite3",
-  connection: {
-    filename: __dirname + "./db/db.sqlite",
-  },
-  useNullAsDefault: true,
-});
-
-
-module.exports = {knex, sqlite};
+CRUD();
